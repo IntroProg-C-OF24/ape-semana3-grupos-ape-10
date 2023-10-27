5cbabc442785966520c0a9728df1bb1d5b87286c
Dario <daminga2@utpl.edu.ec>
2023-10-26 21:08:42 -0500

    Ejercicio 6

--- Ejercicio06/Ejercicio6/src/ejercicio6/Ejercicio6.java
+++ Ejercicio06/Ejercicio6/src/ejercicio6/Ejercicio6.java
@@ -4,12 +4,12 @@ public static void main(String[] args) {
    int val1=81;
    int val2=9;
    int val3=3;
+   int val4=10;
+   int val5=1;
    boolean respuesta;
-   boolean respuesta2;
-   respuesta=((Math.sqrt(val1)+val2)/val3)==9;
-   respuesta2=(10>1);
-    System.out.println("La primera operacion es considerada: "+respuesta);
-    System.out.println("La segunda operacion es considerada: "+ respuesta2);
+   respuesta=((Math.sqrt(val1)+val2)/val3)==9 && (val4>val5);
+    System.out.println("La  operacion es considerada: "+respuesta);
+   
     
     
    
