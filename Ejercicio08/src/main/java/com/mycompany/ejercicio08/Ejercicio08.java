984f291a9feb2eedd67adc6af3150fba611869e6
Mario Morocho <mamorocho33@utpl.edu.ec>
2023-10-30 20:16:15 -0500

    Ejercio 8 listo

---
+++ Ejercicio08/src/main/java/com/mycompany/ejercicio08/Ejercicio08.java
@@ -0,0 +1,19 @@
+package com.mycompany.ejercicio08;
+public class Ejercicio08 {
+    public static void main(String[] args) {
+        int valor01= 10;
+        int valor02= 9;
+        int valor03= 20;
+        int valor04= 500;
+        int valor05= 2;
+        int valor06= 3;
+        int valor07= 200;
+        int valor08= 50;
+        int valor09= 51;
+        boolean respuesta;
+        
+        respuesta = (((valor01 * valor02)+ valor03-valor04/valor05+ Math.pow(valor06, 2)>=valor07)||(valor08>=valor08-valor08+valor09));
+        System.out.println("La respuesta es:" + respuesta);
+        
+    }
+}
